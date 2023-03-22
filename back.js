11beca152c53628ae22008f198532330c7a20909
VictorAlfonsoGarciaUribe <vagu129@hotmail.com>
2023-03-21 20:59:19 -0500

    inicializo variables, botones y agrego arreglo vacio

---
+++ back.js
@@ -0,0 +1,23 @@
+ //definir las variables que van a apuntar a los elementos en html
+
+    let reference   = document.getElementById('reference');
+    let description = document.getElementById('description');
+    let genero   = document.getElementById('genero');
+    let disponible       =document.getElementById('disponible');
+    let nodisponible       =document.getElementById('nodisponible');
+
+ //Mensajes span
+     let mreference = document.getElementById('mreference');
+     let mdescription = document.getElementById('mdescription');
+  
+
+//botones
+     let btnadd    = document.getElementById('btnadd');     
+     let btnsearch = document.getElementById('btnsearch');
+     let btnupdate = document.getElementById('btnupdate');
+     let btndelete = document.getElementById('btndelete');
+     let btnlist   = document.getElementById('btnlist');
+
+//Definir el arreglo de objetos para los productos
+     let libros = [
+     ];
