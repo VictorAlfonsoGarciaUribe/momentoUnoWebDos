d3c2aebffd46414e31f8ae8fed7d6674fa9e7ee1
VictorAlfonsoGarciaUribe <vagu129@hotmail.com>
2023-03-21 22:00:03 -0500

    Revert "inicia codigo js en index html"
    This reverts commit 6131a649d2909ecb60aca1c2e967c4905e49b544.

--- back.js
+++ back.js
@@ -1,47 +1,23 @@
- //Definir el arreglo de objetos para los productos
- let libros = [
-];
- 
-//botones
-let btnadd    = document.getElementById('btnadd');     
-let btnsearch = document.getElementById('btnsearch');
-let btnupdate = document.getElementById('btnupdate');
-let btndelete = document.getElementById('btndelete');
-let btnlist   = document.getElementById('btnlist');
+ //definir las variables que van a apuntar a los elementos en html
 
+    let reference   = document.getElementById('reference');
+    let description = document.getElementById('description');
+    let genero   = document.getElementById('genero');
+    let disponible       =document.getElementById('disponible');
+    let nodisponible       =document.getElementById('nodisponible');
 
-//definir las variables que van a apuntar a los elementos en html
-let reference= document.getElementById('reference').value;
-let description=document.getElementById('description').value;
-let genero= document.getElementById('genero').value;
-let disponible=document.getElementById('disponible').value;
-let nodisponible       =document.getElementById('nodisponible').value;
-
-//Mensajes span
-let mreference = document.getElementById('mreference');
-let mdescription = document.getElementById('mdescription');
-
-
-
-//Evento click del boton agregar 
-btnadd.addEventListener('click', ()=>{
-
-
-        
-        alert (reference);
-        
-       
-            
-      })
-
-
-
+ //Mensajes span
+     let mreference = document.getElementById('mreference');
+     let mdescription = document.getElementById('mdescription');
   
 
+//botones
+     let btnadd    = document.getElementById('btnadd');     
+     let btnsearch = document.getElementById('btnsearch');
+     let btnupdate = document.getElementById('btnupdate');
+     let btndelete = document.getElementById('btndelete');
+     let btnlist   = document.getElementById('btnlist');
 
-
-
-
-     let capturaref= reference;
-
-   
+//Definir el arreglo de objetos para los productos
+     let libros = [
+     ];
